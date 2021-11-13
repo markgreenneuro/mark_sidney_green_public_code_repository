838f06f121cdf2f1a9b4cb79dfef901a66181f3d
markgreenneuro <77732737+markgreenneuro@users.noreply.github.com>
2021-11-13 00:19:59 +0000

    Add files via upload

---
+++ ML/PRELIMINARY_LOG_REG/PRELIMINARY_LOG_REG_CF/RUN_GENERATE_PRELIMINARY_LOG_REG_ONE_CF.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -l
+#SBATCH --exclusive
+#SBATCH -c 6
+#SBATCH --mem=59000
+#SBATCH --job-name=generate_preliminary_log_reg_cf
+#SBATCH --time=0-00:05:00
+#SBATCH --partition=partition-1
+#SBATCH --ntasks=1
+#SBATCH --output=/home/markgreenneuroscience_gmail_com/LOG/ML/PRELIMINARY_LOG_REG/PRELIMINARY_LOG_REG_CF/generate_preliminary_log_reg_cf-%j.output
+#SBATCH --error=/home/markgreenneuroscience_gmail_com/LOG/ML/PRELIMINARY_LOG_REG/PRELIMINARY_LOG_REG_CF/generate_preliminary_log_reg_cf-%j.error
+
+wd=/home/markgreenneuroscience_gmail_com/ML/PRELIMINARY_LOG_REG/PRELIMINARY_LOG_REG_CF
+
+source /etc/bash.bashrc
+eval "$(conda shell.bash hook)"
+conda activate python3710
+
+cd $wd || exit
+python3 GENERATE_PRELIMINARY_LOG_REG_CF.py
